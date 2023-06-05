eb73bea21d893ad8236557d3c17de04ac8622c9e
123 <@Prolab07 Prolab07>
2023-06-05 19:08:23 +0200

    Initial commit

--- mane.java
+++ mane.java
@@ -3,3 +3,14 @@ public class mane {
         System.out.println("Hello WorLd");
     }
 }
+
+public class mane {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        System.out.println(a + b);
+        System.out.println(b - a);
+        System.out.println(b / a);
+        System.out.println(b * a);
+    }
+}
