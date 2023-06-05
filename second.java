364f61ecd552ab7731d4482cbf9e422f1d6a7111
123 <@Prolab07 Prolab07>
2023-06-05 20:45:50 +0200

    dz 2

---
+++ second.java
@@ -0,0 +1,10 @@
+public class second {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(b-a);
+        System.out.println(b/a);
+        System.out.println(b*a);
+    }
+}
