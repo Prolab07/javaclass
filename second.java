2eea535cebfb3889632c4a34b3a33796e6db7ba9
123 <@Prolab07 Prolab07>
2023-06-06 19:38:43 +0200

    dz 2

--- second.java
+++ second.java
@@ -1,10 +1,45 @@
+
+/*
 public class second {
     public static void main(String[] args) {
         int a = 3;
         int b = 5;
-        System.out.println(a+b);
-        System.out.println(b-a);
-        System.out.println(b/a);
-        System.out.println(b*a);
+        System.out.println(a + b);
+        System.out.println(b - a);
+        System.out.println(b / a);
+        System.out.println(b * a);
     }
 }
+*/
+
+public class second {
+    public static void main(String[] args) {
+        float a = 356.08F;
+        float b = 5443.64F;
+        System.out.println(a + b);
+        System.out.println(b - a);
+        System.out.println(b / a);
+        System.out.println(b * a);
+    }
+}
+/*
+public class second {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int result1 = a + b;
+        int result2 = a - b;
+        int result3 = a * b;
+        int result4 = b / a;
+        System.out.println("rezult");
+        System.out.println("dodavannay");
+        System.out.println(result1);
+        System.out.println("vidnimanay");
+        System.out.println(result2);
+        System.out.println("mnozhennay");
+        System.out.println(result3);
+        System.out.println("dilennay");
+        System.out.println(result4);
+    }
+}
+*/
