83463d116348c41a291e849e543fb4a81f385b5e
123 <@Prolab07 Prolab07>
2023-06-08 15:51:02 +0200

    dz3

---
+++ third.java
@@ -0,0 +1,51 @@
+/*
+public class third {
+    public static void main(String[] args) {
+        String l1 = "olena";
+        System.out.println(l1.toUpperCase());
+    }
+}
+*/
+
+/*
+public class third {
+    public static void main(String[] args) {
+        String l1 = "OLENA";
+        System.out.println(l1.toLowerCase());
+    }
+}
+*/
+
+/*
+public class third {
+     public static void main(String[] args) {
+        String t1 = "real";
+        System.out.println(t1.charAt(0));
+    }
+}
+*/
+
+/*public class third {
+    public static void main(String[] args) {
+        String t1 = "real";
+        System.out.println(t1.charAt(3));
+    }
+}
+*/
+
+/*
+public class third {
+    public static void main(String[] args) {
+        String t1 = "horosha";
+        System.out.println(t1.lastIndexOf("h"));
+    }
+}
+*/
+
+public class third {
+    public static void main(String[] args) {
+        String t1 = "horosha";
+        System.out.println(t1.indexOf("h"));
+    }
+}
+
