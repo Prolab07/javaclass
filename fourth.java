f880f04d6c9428a0d65c794a98550a488aa54285
123 <@Prolab07 Prolab07>
2023-06-12 21:58:58 +0200

    dz4

---
+++ fourth.java
@@ -0,0 +1,88 @@
+/*
+public class fourth {
+    public static void main(String[] args) {
+    int i = 1;
+        System.out.println("ifMethod starts");
+        if (i>0) {
+            System.out.println("i is positive");
+        }
+        System.out.println("ifMethod ends");
+    }
+}
+*/
+
+/*
+ public class fourth {
+   public static void main(String[] args) {
+    int i = 0;
+        System.out.println("ifMethod starts");
+        if (i>0) {
+            System.out.println("i is positive");
+        }
+        System.out.println("ifMethod ends");
+    }
+}
+     */
+/*
+public class fourth {
+    public static void main(String[] args) {
+        ifElseMethod(1);
+        ifElseMethod(-1);
+    }
+        private static void ifElseMethod(int i) {
+            System.out.println("ifElseMethod starts");
+            if (i > 0) {
+                System.out.println("i is positive");
+            } else {
+                System.out.println("i is negative");
+            }
+            System.out.println("ifElseMethod ends");
+        }
+}
+*/
+/*
+public class fourth {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        if (a>b) {
+            System.out.println("a>b"); {
+            }
+        } else {
+                System.out.println("b>a");
+        }
+        System.out.println("ifMethod ends");
+    }
+}
+ */
+
+
+public class fourth {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        if (a<b) {
+            System.out.println("a<b"); {
+            }
+        } else {
+            System.out.println("b<a");
+        }
+        System.out.println("ifElseMethod ends");
+    }
+}
+
+/*
+public class fourth {
+    public static void main(String[] args) {
+        int a = 2;
+        if (a % 2 == 0) {
+            System.out.println("a chotnoe"); {
+            }
+        } else {
+            System.out.println("a nechotnoe");
+        }
+        System.out.println("ifElseMethod ends");
+    }
+}
+*/
+
