e925b9aab4f908d077b47131eb6a1db12fe55009
123 <@Prolab07 Prolab07>
2023-06-16 21:34:45 +0200

    dz5

---
+++ fifth.java
@@ -0,0 +1,81 @@
+/*
+public class fifth {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+*/
+
+/*
+public class fifth {
+    public static void main(String[] args) {
+        for (int i=10; i<=90; i= i+3) {
+        System.out.println(i);
+        }
+    }
+    }
+ */
+/*
+public class fifth {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i< 150) {
+            System.out.println(i);
+            i= i+3;
+        }
+    }
+}
+*/
+
+
+/*
+public class fifth {
+    public static void main(String[] args) {
+        int i = 20;
+        do {
+            System.out.println(i);
+            i=i+10;
+        } while (i <= 200);
+    }
+}
+*/
+
+/*
+    public class fifth {
+        public static void main(String[] args) {
+            for (int i = 1; i <= 100; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+ */
+
+
+  public class fifth {
+      public static void main(String[] args) {
+          int a1 = 0, a2 = 1, a3 = 0;
+          while (a3 <= 100) {
+              System.out.println(a3);
+              a3 = a1 + a2;
+              a1 = a2;
+              a2 = a3;
+          }
+      }
+  }
+
+   /*
+   public class fifth {
+     public static void main(String[] args) {
+         int a = 1;
+         for (int i = 1; i <= 7; i++) {
+             a *= i;
+         }
+             System.out.println(a);
+         }
+     }
+     */
+
