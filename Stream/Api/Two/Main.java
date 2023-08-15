d22c372531155d62d8a6172f7fa3634ca3018d13
123 <@Prolab07 Prolab07>
2023-08-15 22:04:14 +0200

    dz-17

---
+++ Stream/Api/Two/Main.java
@@ -0,0 +1,41 @@
+package Stream.Api.Two;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+//задача создать лист с повторяющимися значениями и вывести количество их повторений
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> pib = new ArrayList<>();
+
+        pib.add("Antonov");
+        pib.add("Popov");
+        pib.add("Zobov");
+        pib.add("Portos");
+        pib.add("Aramis");
+        pib.add("Zyev");
+        pib.add("Kotov");
+        pib.add("Portos");
+        pib.add("Aramis");
+        pib.add("Zyev");
+        pib.add("Kotov");
+        pib.add("Antonov");
+        pib.add("Popov");
+        pib.add("Zobov");
+        pib.add("Portos");
+        pib.add("Aramis");
+        pib.add("Zyev");
+        pib.add("Kotov");
+        pib.add("Portos");
+        pib.add("Aramis");
+        pib.add("Pantov");
+
+        System.out.println(pib);
+        Map<String,Long> countOfSurname = pib.stream().collect(Collectors.groupingBy(surname->surname, Collectors.counting()));
+        System.out.println(countOfSurname);
+
+}
+}
---
+++ Stream/Api/Two/RandomApi.java
@@ -0,0 +1,24 @@
+package Stream.Api.Two;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+//задача создать лист с рандомными значениями и вывести их сумму
+public class RandomApi {
+    private static int random() {
+        Random random = new Random();
+        return random.nextInt(100);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> randomlist = List.of(random(),random(),random(),random());
+        //List<Integer> randomlistone = new ArrayList<>(random()); // надо разобраться
+        Optional<Integer> sum = randomlist.stream().reduce((a, b) -> a+b);
+        System.out.println(sum.get());
+        System.out.println("Сумма" + " " + sum.get());
+
+        System.out.println(randomlist);
+        //System.out.println(randomlistone);
+    }
+}
